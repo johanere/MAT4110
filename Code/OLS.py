f393e1ebfbf6d98e6c3598aba6dbc07baa8a9c88
johanere <42669090+johanere@users.noreply.github.com>
2019-09-23 12:25:31 +0200

    Add files via upload

--- Code/OLS.py
+++ Code/OLS.py
@@ -1,51 +1,160 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+from random import random, seed
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split, cross_validate
+import sklearn.linear_model as skl
+from sklearn.model_selection import KFold
 
 
-class OLS:
-    def __init__(self, m=3, n=6, set=0):
-        self.m = m
-        self.n = n
-        self.set = set
+def FrankeFunction(x, y):
+    term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+    term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+    term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+    term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
+    return term1 + term2 + term3 + term4
 
-    def construct_matr(self):
+
+def r2(z1, z2):
+    r2 = r2_score(z1, z2, sample_weight=None, multioutput="uniform_average")
+    return r2
+
+
+def MSE(z1, z2):
+    MSE = mean_squared_error(z1, z2, sample_weight=None, multioutput="uniform_average")
+    return MSE
+
+
+class Data:
+    def __init__(self, p=5,eps=1):
+        """Initiates class. Provide degree of polynomial, p"""
+        self.p = p
+        self.eps=eps
+
+    def GenerateData_1(self):
+        """Generates a random NxN-grid and computes targets [with noise] from Franke Function. Provide N and noise [true]/false"""
+        n =30   # Total number of data points
+        eps = self.eps
         start = -2
         stop = 2
-        x = np.linspace(start, stop, self.n)
-        eps = 1
-        r = np.random.rand(self.n) * eps
-        self.A = np.ones((self.n, self.m))
-        if set == 0:
-            y = x * (np.cos(r + 0.5 * x ** 3) + np.sin(0.5 * x ** 3))
-        else:
-            y = 4 * x ** 5 - 5 * x ** 4 - 20 * x ** 3 + 10 * x ** 2 + 40 * x + 10 + r
-        for i in range(1, self.m):
-            self.A[:, i] = x ** i
+        x = np.linspace(start,stop,n)
+        self.x=x
+        r = np.random.uniform(0, 1, n) * eps
+        self.y = x*(np.cos(r+0.5*x**3)+np.sin(0.5*x**3))
+        self.n=n
 
-    def QR(self):
-        q, self.r = np.linalg.qr(self.A)
+    def GenerateData_2(self):
+        n = 30
+        eps = self.eps
+        start = -2
+        stop = 2
+        x = np.linspace(start,stop,n)
+        self.x=x
+        r = np.random.uniform(0, 1, n) * eps
+        self.y = 4*x**5- 5*x**4 - 20*x**3 + 10*x**2 + 40*x + 10 + r
+        self.n=n
+
+    def CreateDesignMatrix(self):
+        """  Sets up design matrix X. Splits X in training and test data"""
+        x = self.x
+        y = self.y
+        X = np.ones((self.n, self.p))
+        for i in range(1,self.p):
+            X[:,i]=x**i
+        self.X = X
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, self.y, test_size=0.2)
+
+    def QRsolve(self):
+        q,r=np.linalg.qr(self.X)
+        qt=np.dot(q.T,self.y)
+        p=self.p
+        self.beta=np.zeros(self.p)
+        self.beta[p-1]=qt[self.p-1]/r[p-1,p-1]
+        for i in range(self.p-2,-1,-1):
+            sum=0
+            for j in range(i,p):
+                sum+=r[i,j]*self.beta[j]
+            self.beta[i]=(qt[i]-sum)/r[i,i]
+        self.ypred=np.dot(self.X, self.beta)
+        print(self.beta)
+
+    def OLS(self):
+        """Stores a beta vector using OLS on currently set training data"""
+        X = self.X_train
+        z = self.z_train
+        self.beta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(z)
+
+    def Ridge(self, ridge_lambda=0):
+        """Stores a beta vector using Ridge on currently set training data. Provide Lambda"""
+        X = self.X_train
+        z = self.z_train
+        dim = np.shape(np.linalg.pinv(X.T.dot(X)))[0]
+        self.beta = (
+            np.linalg.pinv((X.T.dot(X)) + np.identity(dim) * ridge_lambda)
+            .dot(X.T)
+            .dot(z)
+        )
+
+    def Train_Predict(self):
+        """Target prediction utilizing training data and current beta. Stores MSE and r2 scores"""
+        self.z_train_predict = np.dot(self.X_train, self.beta)
+        self.MSE_train = MSE(Data.z_train, self.z_train_predict)
+        self.r2_train = 0
+        if self.n > 4:
+            self.r2_train = r2(Data.z_train, self.z_train_predict)
+
+    def Test_Predict(self):
+        """Target prediction utilizing test data and current beta. Stores MSE and r2 scores"""
+        self.z_test_predict = np.dot(self.X_test, self.beta)
+        self.MSE_test = MSE(self.z_test, self.z_test_predict)
+        self.r2_test = 0
+        if self.n > 4:
+            self.r2_test = r2(self.z_test, self.z_test_predict)
+
+    def Skl_OLS(self, k=4):
+        clf = skl.LinearRegression().fit(self.X, self.y)
+        self.ytilde = clf.predict(self.X)
 
 
-    def BWsub(self):
-        self.c=self.q.T.dot(self.y)
-        for i in range(0, self.m):
-            if abs(self.r[i, i]) < 1.0e-12:
-                print("In QR: R is singular. Program terminated.")  # FIKS DETTE  TRENGS DETTE=
-                break
-        #self.b = np.zeros(self.n)
-        #b[n - 1] = self.y[n - 1] / self.q[n, m]
+    def Skl_Lasso(self, k=10):
+        lasso = skl.Lasso().fit(self.X_train, self.z_train)
+        ytilde = lasso.predict(self.X_train)
+        self.MSE_SKL_Lasso = MSE(Data.z_train, ytilde)
+        self.r2_SKL_Lasso = 0
+        if self.n > 4:
+            self.r2_SKL_Lasso = r2(Data.z_train, ytilde)
+        cv_results = cross_validate(lasso, self.X, self.z, cv=k)
+        print("Lasso score \n", cv_results["test_score"])
+
+    def Kfold_Crossvalidation(self, method=0, llambda=0, k=10):
+        "k fold cross validation. 0-OLS, 1-Ridge. Gets MSE from test predict"
+        self.kscores = np.zeros((2, k))
+        kfold = KFold(k, shuffle=True)
+        j = 0
+        for train_inds, test_inds in kfold.split(self.X):
+            self.X_train, self.X_test = self.X[train_inds], self.X[test_inds]
+            self.z_train, self.z_test = self.z[train_inds], self.z[test_inds]
+            if method == 0:
+                self.OLS()
+            if method == 1:
+                self.Ridge(llambda)
+            self.Train_Predict()
+            self.Test_Predict()
+            self.kscores[0, j] = self.MSE_test
+            self.kscores[1, j] = self.r2_test
+            j += 1
 
 
-matr = OLS()
-matr.construct_matr()
-matr.QR()
-matr.BWsub()
-print(matr.c)
-
-"""
-A = construct_matr(n, m, 0)
-q, r = np.linalg.qr(A)
-
-print(q)
-print(r)
-"""
+np.random.seed(3)
+data=Data(p=5,eps=0)
+data.GenerateData_1()
+data.CreateDesignMatrix()
+data.QRsolve()
+data.Skl_OLS()
+plt.scatter(data.x,data.y)
+plt.plot(data.x,data.ytilde)
+plt.plot(data.x,data.ypred)
+plt.show()
--- Code/QR.py
+++ Code/QR.py
@@ -1,38 +1,113 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
-class OLS:
-    def __init__(self, m=3, n=6, set=0):
-        self.m = m
-        self.n = n
-        self.set = set
-        self.A=np.ones((n,m))
+import numpy as np
+from random import random, seed
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split, cross_validate
+import sklearn.linear_model as skl
+from sklearn.model_selection import KFold
 
-    def construct_matr(self):
-        start = -2
-        stop = 2
-        x = np.linspace(start, stop, self.n)
-        eps = 1
-        r = np.random.rand(self.n) * eps
-        if set == 0:
-            y = x * (np.cos(r + 0.5 * x ** 3) + np.sin(0.5 * x ** 3))
-        else:
-            y = 4 * x ** 5 - 5 * x ** 4 - 20 * x ** 3 + 10 * x ** 2 + 40 * x + 10 + r
-        for i in range(1, self.m):
-            self.A[:, i] = x ** i
+
+def FrankeFunction(x, y):
+    term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+    term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+    term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+    term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
+    return term1 + term2 + term3 + term4
+
+
+def r2(z1, z2):
+    r2 = r2_score(z1, z2, sample_weight=None, multioutput="uniform_average")
+    return r2
+
+
+def MSE(z1, z2):
+    MSE = mean_squared_error(z1, z2, sample_weight=None, multioutput="uniform_average")
+    return MSE
+
+
+class Data:
+    def __init__(self, p=5, k=10):
+        self.p = p  # Degree of polynomial
+        self.kfolds = k
+
+    def GenerateData_1(self):
+        N=30
+        start = -2;
+        stop = 2;
+        x = np.linspace(start,stop,n);
+        eps = 1;
+        r = rand(1,n) * eps;
+        print(r)
+
+    def CreateDesignMatrix_X(self):
+        """
+    	Function for creating a design X-matrix with rows [1, x, y, x^2, xy, xy^2 , etc.]
+    	Input is x and y mesh or raveled mesh, keyword agruments n is the degree of the polynomial you want to fit.
+    	"""
+        x = self.x
+        y = self.y
+        p = self.p
+        N = len(x)
+        l = int((p + 1) * (p + 2) / 2)  # Number of elements in beta
+
+        X = np.ones((N, l))
+        for i in range(1, p + 1):
+            q = int((i) * (i + 1) / 2)
+            for k in range(i + 1):
+                X[:, q + k] = x ** (i - k) * y ** k
+            self.X = X
+            self.X_train, self.X_test, self.z_train, self.z_test = train_test_split(
+                X, self.z, test_size=0.2
+            )
+
+    def Train(self):
+        X = self.X_train
+        z = self.z_train
+        self.beta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(z)
+
+    def Predict(self):
+        self.z_train_predict = np.dot(self.X_train, self.beta)
+        self.MSE_train = MSE(Data.z_train, self.z_train_predict)
+        self.r2_train = 0
+        if self.n > 4:
+            self.r2_train = r2(Data.z_train, self.z_train_predict)
+
+    def Test(self):
+        self.z_test_predict = np.dot(self.X_test, self.beta)
+        self.MSE_test = MSE(self.z_test, self.z_test_predict)
+        self.r2_test = 0
+        if self.n > 4:
+            self.r2_test = r2(self.z_test, self.z_test_predict)
+
+    def SklReg(self):
+        clf = skl.LinearRegression().fit(self.X_train, self.z_train)
+        self.ytilde = clf.predict(self.X_train)
+        self.MSE_SKL = MSE(Data.z_train, Data.ytilde)
+        self.r2_SKL = 0
+        if self.n > 4:
+            self.r2_SKL = r2(Data.z_train, Data.ytilde)
+        cv_results = cross_validate(clf, self.X, self.z, cv=self.kfolds)
+        print(cv_results["test_score"])
+
+    def xval(self):
+        k = self.kfolds
+        self.kscores = np.zeros((2, k))
+        kfold = KFold(k, shuffle=True)
+        j = 0
+        for train_inds, test_inds in kfold.split(self.X):
+            self.X_train, self.X_test = self.X[train_inds], self.X[test_inds]
+            self.z_train, self.z_test = self.z[train_inds], self.z[test_inds]
+            self.Train()
+            self.Predict()
+            self.Test()
+            self.kscores[0, j] = self.MSE_test
+            self.kscores[1, j] = self.r2_test
+            j += 1
+
     def QR(self):
-
-
-matr=OLS()
-matr.construct_matr()
-print(matr.A)
-
-
-
-"""
-A = construct_matr(n, m, 0)
-q, r = np.linalg.qr(A)
-
-print(q)
-print(r)
-"""
+        q, r = np.linalg.qr(self.X)
+        print(r)
